f1f9af3c10ac4627b63cd0adf3555706971bb0ff
MWolff89 <150514371+MWolff89@users.noreply.github.com>
2023-11-27 04:22:48 +0000

    new

--- components/header.tsx
+++ components/header.tsx
@@ -72,10 +72,10 @@ export async function Header() {
 
       </div>
       <div className="flex items-center justify-end space-x-2">
-        <Button onClick={() => uploadPDF()}>
+        {/* <Button onClick={() => uploadPDF()}>
           <IconGitHub />
           <span className="hidden ml-2 md:flex">Upload PDF</span>
-        </Button>
+        </Button> */}
         <a
           target="_blank"
           href="https://tailwind-nextjs-starter-blog-eight-gamma.vercel.app/"
